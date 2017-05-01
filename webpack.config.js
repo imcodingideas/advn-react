02f3bced195abc0ee9739965b234424b0deb1f56
Joseph Chambers <joseph@michael-chambers.com>
2017-05-01 10:26:47 -0700

    added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ module.exports = {
   },
   module: {
   rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
       }
-    }
-  ]
-}
+    ]
+  }
 };
